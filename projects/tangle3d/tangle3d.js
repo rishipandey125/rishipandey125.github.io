191f681a88a17c497597480ba7ac67eafb31752c
Rishi <rishipandey125@gmail.com>
2022-03-04 18:11:30 -0800

    working on tangle3D

--- projects/tangle3d/tangle3d.js
+++ projects/tangle3d/tangle3d.js
@@ -1,33 +1,32 @@
-const renderer = new THREE.WebGLRenderer(); //
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer(); //render using webgl
+renderer.setSize( window.innerWidth, window.innerHeight ); //set the render size
+document.body.appendChild( renderer.domElement ); //adds a canvas element to html so that we can view the rendered scene
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 ); // create a camera with (fov, aspect ratio, near, far)
+camera.position.set( 0, 0, 100 ); //set the camera location
+camera.lookAt( 0, 0, 0 ); //set camera lookat 
 
-const scene = new THREE.Scene();
-// const geometry = new THREE.BoxGeometry();
-const material = new THREE.LineBasicMaterial( { color: "rgb(255,255,255)" } );
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0) );
+const scene = new THREE.Scene(); //create a scene : stores items (geometry/lights/etc.)
+
+const material = new THREE.LineBasicMaterial( { color: "rgb(255,255,255)" } ); // create a material for the lines
+
+const points = []; //create a list of points for the line
+points.push( new THREE.Vector3( - 10, 0, 0) ); //add some points
 points.push( new THREE.Vector3( 0, 10, 0 ) );
 points.push( new THREE.Vector3( 10, 0, 0 ) );
 
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const geometry = new THREE.BufferGeometry().setFromPoints( points ); //turn the points into geometry by storing in a buffer geo
 
-const line = new THREE.Line( geometry, material );
+const line = new THREE.Line( geometry, material ); //create a line based on that buffer geo and the material
 
-// scene.add( cube );
 
-scene.add( line );
-// renderer.render( scene, camera );
-// camera.position.z = 5;
+scene.add( line ); // add the line to scene
 
+//animate function onUpdate
 function animate() {
-  requestAnimationFrame( animate );
+  requestAnimationFrame( animate ); //animate function
 
-  // line.rotation.x += 0.01;
+  //rotate line
   line.rotation.y += 0.01;
 
   renderer.render( scene, camera );
