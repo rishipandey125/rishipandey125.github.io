569d5a1fb65ca2d6424233ad79cdb4da23201880
Rishi <rishipandey125@gmail.com>
2022-03-08 00:10:14 -0800

    rotating around sketch

--- projects/tangle3d/tangle3d.js
+++ projects/tangle3d/tangle3d.js
@@ -1,35 +1,43 @@
-const renderer = new THREE.WebGLRenderer(); //render using webgl
-renderer.setSize( window.innerWidth, window.innerHeight ); //set the render size
-document.body.appendChild( renderer.domElement ); //adds a canvas element to html so that we can view the rendered scene
+var renderer, camera, scene;//gl renderer, camera and the scene
+var geometry, line; //geometry and line
+var rotation = 0; //initial rotation
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 ); // create a camera with (fov, aspect ratio, near, far)
-camera.position.set( 0, 0, 100 ); //set the camera location
-camera.lookAt( 0, 0, 0 ); //set camera lookat 
+init();
+animate();
 
-const scene = new THREE.Scene(); //create a scene : stores items (geometry/lights/etc.)
+function init() {
+  renderer = new THREE.WebGLRenderer(); //render using webgl
+  renderer.setSize( window.innerWidth, window.innerHeight ); //set the render size
+  document.body.appendChild( renderer.domElement ); //adds a canvas element to html so that we can view the rendered scene
+  
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 ); // create a camera with (fov, aspect ratio, near, far)
+  camera.position.set( 0, 0, 100 ); //set the camera location
+  camera.lookAt( 0, 0, 0 ); //set camera lookat 
+  
+  scene = new THREE.Scene(); //create a scene : stores items (geometry/lights/etc.)
+  
+  const material = new THREE.LineBasicMaterial( { color: "rgb(255,255,255)" } ); // create a material for the lines
+  const points = []; //create a list of points for the line
+  for (let i = 0; i < 50; i++) {
+    points.push(new THREE.Vector3(getRandomNumber(-20,20),getRandomNumber(-20,20),getRandomNumber(-20,20))) // push a random point
+  }
+  geometry = new THREE.BufferGeometry().setFromPoints( points ); //turn the points into geometry by storing in a buffer geo
 
-const material = new THREE.LineBasicMaterial( { color: "rgb(255,255,255)" } ); // create a material for the lines
+  line = new THREE.Line( geometry, material ); //create a line based on that buffer geo and the material
+  scene.add(line);
+}
 
-const points = []; //create a list of points for the line
-points.push( new THREE.Vector3( - 10, 0, 0) ); //add some points
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points ); //turn the points into geometry by storing in a buffer geo
-
-const line = new THREE.Line( geometry, material ); //create a line based on that buffer geo and the material
-
-
-scene.add( line ); // add the line to scene
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 //animate function onUpdate
 function animate() {
   requestAnimationFrame( animate ); //animate function
 
   //rotate line
-  line.rotation.y += 0.01;
+  rotation += 0.001
+  line.rotation.y = rotation;
 
   renderer.render( scene, camera );
-};
-
-animate();
+}
