a2a322c066d0d32e1543f17df800023f04d750bc
rishipandey125 <35504258+rishipandey125@users.noreply.github.com>
2022-04-11 12:34:20 -0700

    understanding semantic breakdown for facemesh

--- projects/genport/gen_port.js
+++ projects/genport/gen_port.js
@@ -1,16 +1,4 @@
-// var img;
-// //setup the canvas for rendering
-
-
-
-// function setup() {
-//   createCanvas(windowWidth,windowHeight) //create the canvas w/ TT size
-//   // background(255, 204, 0);
-//   image(img, 0, 0);
-// }
-
-
-let facemesh;
+let facemesh; 
 let predictions = [];
 let img;
 var px = 0;
@@ -22,15 +10,11 @@ function preload() {
 
 function setup() {
   // Create a canvas that's at least the size of the image.
-  createCanvas(img.width,img.height) //create the canvas w/ TT size
-
-  // create an image using the p5 dom library
-  // call modelReady() when it is loaded
-  // img = createImg("https://corsanywhere.herokuapp.com/http://thispersondoesnotexist.com/image", imageReady);
-  // set the image size to the size of the canvas
+  createCanvas(img.width,img.height)
+  background('black')
   imageReady()
-  // img.hide(); // hide the image in the browser
-  frameRate(1); // set the frameRate to 1 since we don't need it to be running quickly in this case
+
+  // frameRate(1); // set the frameRate to 1 since we don't need it to be running quickly in this case
 }
 
 // when the image is ready, then load up poseNet
@@ -44,14 +28,14 @@ function imageReady() {
 
 // when poseNet is ready, do the detection
 function modelReady() {
-  console.log("Model ready!");
+  // console.log("Model ready!");
   facemesh.predict(img);
 }
 
 // draw() will not show anything until poses are found
 function draw() {
   if (predictions.length > 0) {
-    // image(img, 0, 0, width, height);
+    image(img, 0, 0, width, height);
     drawKeypoints();
     noLoop(); // stop looping when the poses are estimated
   }
@@ -60,19 +44,32 @@ function draw() {
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
   //create a new stroke
-  // stroke("red")
+  stroke("red")
   for (let i = 0; i < predictions.length; i += 1) {
-    const keypoints = predictions[i].scaledMesh;
-    // [px, py] = keypoints[0];
+    // const keys = Object.keys(predictions[i].annotations);
+    // console.log(keys)
+    Object.entries(predictions[i].annotations).forEach(([key, keyPoints]) => {
+      // console.log(key);
+      // loop through every value and draw a new line of random color
+      px =  keyPoints[0].x;
+      py =  keyPoints[0].y;
+      for (j = 0; j < keyPoints.length; j++ ) {
+        const [x, y] = keyPoints[j];
+        line(px,py,x,y);
+        px = x;
+        py = y;
+      }
+    });
+
+    const keyPointsScaled = predictions[i].scaledMesh;
+
     // Draw facial keypoints.
-    for (let j = 0; j < keypoints.length; j += 1) {
-      const [x, y] = keypoints[j];
-      // console.log([x,y])
-      // line(px,py,x,y)
-      // px = x;
-      // py = y;
-      fill(255*(j/keypoints.length));
-      ellipse(x, y, 10, 10);
+    for (let j = 0; j < keyPointsScaled.length; j += 1) {
+      const [x, y] = keyPointsScaled[j];
+
+      fill(0, 255, 0);
+      ellipse(x, y, 5, 5);
     }
+
   }
 }
