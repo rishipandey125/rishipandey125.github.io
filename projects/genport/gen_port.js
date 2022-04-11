72b1f71b6154a457eb6c236e78ea265bcbb14244
rishipandey125 <35504258+rishipandey125@users.noreply.github.com>
2022-04-10 23:03:42 -0700

    setting up generative portrait projct

---
+++ projects/genport/gen_port.js
@@ -0,0 +1,7 @@
+var img;
+//setup the canvas for rendering
+function setup() {
+  img = loadImage('http://www.rishi-pandey.com/assets/portrait.png');
+  image(img, 0, 0);
+  background(255, 204, 0);
+}
