5909096e2912821024ff4e1746b4bb92a37b0b1f
rishipandey125 <35504258+rishipandey125@users.noreply.github.com>
2022-04-11 20:14:16 -0700

    identifying semantics

--- projects/genport/gen_port.js
+++ projects/genport/gen_port.js
@@ -3,6 +3,7 @@ let predictions = [];
 let img;
 var px = 0;
 var py = 0;
+var semantics = ['silhouette','lipsUpperOuter','lipsLowerOuter','lipsUpperInner','lipsLowerInner']
 
 function preload() {
   img = loadImage('https://corsanywhere.herokuapp.com/http://thispersondoesnotexist.com/image');
@@ -32,10 +33,14 @@ function modelReady() {
   facemesh.predict(img);
 }
 
+function randomColor() {
+  return color(random(0,255),random(0,255),random(0,255));
+}
+
 // draw() will not show anything until poses are found
 function draw() {
   if (predictions.length > 0) {
-    image(img, 0, 0, width, height);
+    // image(img, 0, 0, width, height);
     drawKeypoints();
     noLoop(); // stop looping when the poses are estimated
   }
@@ -44,32 +49,25 @@ function draw() {
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
   //create a new stroke
-  stroke("red")
+  stroke("white")
+  strokeWeight(10)
   for (let i = 0; i < predictions.length; i += 1) {
     // const keys = Object.keys(predictions[i].annotations);
     // console.log(keys)
     Object.entries(predictions[i].annotations).forEach(([key, keyPoints]) => {
-      // console.log(key);
-      // loop through every value and draw a new line of random color
-      px =  keyPoints[0].x;
-      py =  keyPoints[0].y;
-      for (j = 0; j < keyPoints.length; j++ ) {
-        const [x, y] = keyPoints[j];
-        line(px,py,x,y);
-        px = x;
-        py = y;
+      console.log(key);
+      if (semantics.includes(key)) {
+        // loop through every value and draw a new line of random color
+        px =  keyPoints[0].x;
+        py =  keyPoints[0].y;
+        // stroke(randomColor())
+        for (j = 0; j < keyPoints.length; j++ ) {
+          const [x, y] = keyPoints[j];
+          line(px,py,x,y);
+          px = x;
+          py = y;
+        }
       }
     });
-
-    const keyPointsScaled = predictions[i].scaledMesh;
-
-    // Draw facial keypoints.
-    for (let j = 0; j < keyPointsScaled.length; j += 1) {
-      const [x, y] = keyPointsScaled[j];
-
-      fill(0, 255, 0);
-      ellipse(x, y, 5, 5);
-    }
-
   }
 }
