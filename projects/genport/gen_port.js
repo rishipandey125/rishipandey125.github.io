9235f64318e6bfe1c244055f573a0638cfd37eb0
rishipandey125 <35504258+rishipandey125@users.noreply.github.com>
2022-04-13 19:26:33 -0700

    trying to draw wobbly curve

--- projects/genport/gen_port.js
+++ projects/genport/gen_port.js
@@ -3,6 +3,12 @@ let predictions = [];
 let img;
 var px = 0;
 var py = 0;
+
+const pixelsPerSegment = 10;
+const noiseScale = 120;
+const noiseFrequency = 0.01;
+const noiseSpeed = 0.1;
+
 var semantics = [
                 'lipsUpperOuter',
                 'lipsLowerOuter',
@@ -81,7 +87,7 @@ function draw() {
 function drawKeypoints() {
   //create a new stroke
   stroke("white")
-  strokeWeight(10)
+  strokeWeight(1)
   for (let i = 0; i < predictions.length; i += 1) { //for all the predictions - should be 1
     // loop through all semantics - if it is that one draw
     for (let k = 0; k < semantics.length; k++) {
@@ -119,7 +125,9 @@ function drawKeypoints() {
             //loop through array in correct direction
             while (start != end) {
               const [x, y] = keyPoints[start];
-              line(px,py,x,y); //set line
+              // line(px,py,x,y); //set line
+              drawWobblyCurve(px,py,x,y);
+              //create wobbly line 
               px = x; //update prev
               py = y;
               start += iterate;
@@ -129,4 +137,57 @@ function drawKeypoints() {
       });
     }
   }
+}
+
+function drawWobblyCurve(sx,sy,ex,ey) {
+  let start = createVector(sx,sy);
+  let end = createVector(ex,ey);
+  let lineLength = start.dist(end);
+  // Determine the number of segments, and make sure there is at least one.
+  let segments = max(1, round(lineLength / pixelsPerSegment));
+  // Determine the number of points, which is the number of segments + 1
+  let points = 1 + segments;
+  
+  // We need to know the angle of the line so that we can determine the x
+  // and y position for each point along the line, and when we offset based
+  // on noise we do so perpendicular to the line.
+  let angle = atan2(end.y - start.y, end.x - start.x);
+  
+  let xInterval = pixelsPerSegment * cos(angle);
+  let yInterval = pixelsPerSegment * sin(angle);
+  
+  // beginShape();
+  // Always start with the start point
+  // vertex(start.x, start.y);
+  
+  // for each point that is neither the start nor end point
+  for (let i = 1; i < points - 1; i++) {
+    // determine the x and y positions along the straight line
+    let x = start.x + xInterval * i;
+    let y = start.y + yInterval * i;
+    
+    // calculate the offset distance using noice
+    let offset =
+      // The bigger this number is the greater the range of offsets will be
+      noiseScale *
+      (noise(
+        // The bigger the value of noiseFrequency, the more erretically
+        // the offset will change from point to point.
+        i * pixelsPerSegment * noiseFrequency,
+        // The bigger the value of noiseSpeed, the more quickly the curve
+        // fluxuations will change over time.
+        (millis() / 1000) * noiseSpeed
+      ) - 0.5);
+      
+    // Translate offset into x and y components based on angle - 90°
+    // (or in this case, PI / 2 radians, which is equivalent)
+    let xOffset = offset * cos(angle - PI / 2);
+    let yOffset = offset * sin(angle - PI / 2);
+    
+    line(x,y,x+xOffset,y+yOffset);
+    // vertex(x + xOffset, y + yOffset);
+  }
+  
+  // vertex(end.x, end.y);
+  // endShape();
 }
