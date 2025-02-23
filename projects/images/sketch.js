b7d41da5d2b7f0d24b8a9088846d5cc942b86abb
rishi <rishipandey125@gmail.com>
2025-02-23 09:04:59 -0800

    new website changes

--- projects/images/sketch.js
+++ projects/images/sketch.js
@@ -7,6 +7,8 @@ let mult;
 let dotSize;
 let shapeStyle;
 
+let drawCount = 0;
+
 
 function preload() {
   img = loadImage('img/faqir.jpg');
@@ -21,10 +23,10 @@ function setup() {
   let newCanvasY = (windowHeight - img.height) / 2;
 
   canvas.position(newCanvasX,newCanvasY);
-  image(img,0,0)
+  // image(img,0,0)
   noStroke();
-  mult = random() * 0.003
-  dotSize = 5 * random();
+  mult = random() * 0.005
+  dotSize = 1 * random();
   for (let i = 0; i < img.width; i += dotSize) {
     for (let j = 0; j < img.height; j += dotSize) {
 
@@ -41,19 +43,25 @@ function setup() {
 
 function draw() {
   
-  // for (var i = 0; i < points.length; i++) { //loop through every point
-  //   // noStroke() //fill inside
-  //   fill(colors[i]) //use assigned local color to fill 
-    
-  //   var currPoint = points[i] //get the current Point
-    
-  //   var angle = map(noise(currPoint.x * mult,currPoint.y * mult),0,1,0,720) //map perlin noise to an angle (degrees)
+  if (drawCount < 100) {
+    for (var i = 0; i < points.length; i++) { //loop through every point
+      // noStroke() //fill inside
+      fill(colors[i]) //use assigned local color to fill 
+      
+      var currPoint = points[i] //get the current Point
+      
+      var angle = map(noise(currPoint.x * mult,currPoint.y * mult),0,1,0,720) //map perlin noise to an angle (degrees)
 
-  //   //use the angle to find the change in x and change in y
-  //   // set that to be the new points location
-  //   points[i].add(createVector(dotSize/2*cos(angle),dotSize/2*sin(angle))) //update direction w/ vector field
-  //   circle(points[i].x,points[i].y,random()*dotSize);
-  // }
+      //use the angle to find the change in x and change in y
+      // set that to be the new points location
+      points[i].add(createVector(dotSize/2*cos(angle),dotSize/2*sin(angle))) //update direction w/ vector field
+      circle(points[i].x,points[i].y,random()*dotSize);
+      // rect(points[i].x,points[i].y,random()*dotSize);
+
+    }
+  }
+
+  drawCount += 1
   
   
 }
--- projects/sound/sketch.js
+++ projects/sound/sketch.js
@@ -1,28 +1,159 @@
-let monoSynth;
+/**
+ * synthruary day 2
+ * Prompt: arpegiattor 
+ * 
+ * <3 whichlight 
+ * 
+ * 
+ */
+
+let synth;
+let contextStarted;
+let w, h;
+let pattern;
+let feedbackDelay; 
+let g_rate = 1; 
+let g_index=0; 
+let g_x=0; 
+let g_y=1; 
+let size = 100; 
+
+
+function touchStarted() {
+    if (!contextStarted) {
+        Tone.start();
+        setupSynths();
+        // let a = select('#instructions');
+        // a.remove();
+        contextStarted = true;
+    }
+
+    updateSynth();
+    return false;
+}
 
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  // cnv.mousePressed(playSynth);
-
-  monoSynth = new p5.MonoSynth();
+    w = windowWidth;
+    h = windowHeight;
+    colorMode(HSB, 360, 100, 100);
+    createCanvas(w, h);
+    background(60, 30, 100,100);
+    drawTriangle();
 }
 
-function playSynth() {
-  userStartAudio();
-
-  console.log(mouseX)
-  let note = mouseX; // i think hertz goes from 20 to 20,000
-  // note velocity (volume, from 0 to 1)
-  let velocity = random();
-  // time from now (in seconds)
-  let time = 0;
-  // note duration (in seconds)
-  let dur = 1/6;
-
-  monoSynth.play(note, velocity, time, dur);
-}
 
 function draw() {
-  background((mouseX/windowWidth)*255)
-  playSynth()
+    let y = g_y; 
+    let x = g_x;
+
+
 }
+
+function setupSynths() {
+    feedbackDelay = new Tone.PingPongDelay(0.5, 0.5).toDestination();
+    feedbackDelay.wet.value=0;
+    
+    synth = new Tone.MonoSynth().connect(feedbackDelay);
+
+    synth.set({
+        "oscillator" : {
+            "type": "pulse"
+        },
+        "filterEnvelope":
+        {
+            "attack": 0.01, //0.01 
+            "decay": 0.5, //0.5
+            "sustain": 0.2, //0.2
+            "release": 0.0 //0
+        }
+
+    });
+
+    let root = 40;
+    let intervals = [0, 3, 5, 9];
+
+    //add an octave
+    for (i of intervals.slice()) { intervals.push(12 + i); }
+
+    //create notes and turn to values for arp  
+    let notes = intervals.map((i) => root + i);
+    let values = notes.map((n) => Tone.Frequency(n, "midi"));
+
+
+    setInterval(function(){
+        background(60, 30, 100,0.4);
+
+        let note = values[g_index];
+        let time = Tone.now(); 
+        synth.triggerAttackRelease(note, 2, time);
+       
+        let x = floor(100*g_x/10);
+        let size= max(5,g_y*h/50);
+        let r = min(w,h)*g_index/values.length;
+
+        strokeWeight(size);
+        for(let i = 0; i<values.length; i++){
+            stroke(300,100,100);
+            noFill();
+            ellipse(w/2-(i*g_x*100),h/2,r,r);
+            ellipse(w/2+(i*g_x*100),h/2,r,r);
+            
+        }
+      
+
+        g_index++;
+        g_index%=values.length;
+    }, 100)
+
+    Tone.Transport.start();
+
+}
+
+
+
+function mouseDragged(){
+    if(contextStarted){
+        updateSynth(); 
+    }
+}
+
+function updateSynth(){
+        let x = mouseX;
+        let y = mouseY;
+        let len = map(abs(y-h/2), 0, h/2, 0.1, 1);
+        let wet = map(abs(w/2-x),0,w/2,0,0.5);
+        feedbackDelay.wet.value = wet;
+        g_x = feedbackDelay.wet.value; 
+        g_y = map(abs(h/2-y), h/2, 0, 1, 0); 
+        synth.set({
+            "envelope":
+            {
+                "decay": len,
+                "sustain": len,
+                "release": len
+            }
+    
+        });
+    
+}
+
+function drawTriangle(){
+    push();
+    translate(width * 0.5, height * 0.5);
+    fill(180,100,100);
+    noStroke();
+    polygon(0, 0, 50, 3);
+    pop(); 
+}
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+
